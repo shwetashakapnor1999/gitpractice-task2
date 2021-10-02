88ed48fb963c8b9faad257a204c522562b126ae9
Shweta Shakapnor <shwetashakapnor1999@gmail.com>
2021-10-02 11:28:02 +0530

    Added fizzbuzz.js file

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+//fizbuzz program in js
+function fizzBuzz(start, end) {
+  for (let num = start; num <= end; num++) {
+    if (num % 5 === 0 && num % 3 === 0) {
+      console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+      console.log("Fizz");
+    } else if (num % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(num);
+    }
+  }
+}
+fizzBuzz(1, 15);
